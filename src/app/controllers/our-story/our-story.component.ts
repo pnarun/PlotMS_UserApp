b64bbef6b3858761e3ba8be28d4cf752be48423b
Arun P N <arunpn866@gmail.com>
2025-01-19 15:08:55 +0530

    Our story is done

--- src/app/controllers/our-story/our-story.component.ts
+++ src/app/controllers/our-story/our-story.component.ts
@@ -1,27 +1,12 @@
 import { Component } from '@angular/core';
+import { CommonModule } from '@angular/common';
 
 @Component({
   selector: 'app-our-story',
   standalone: true,
-  template: `
-    <div class="coming-soon">
-      <h1>Coming soon!!!</h1>
-    </div>
-  `,
-  styles: [`
-    .coming-soon {
-      height: 100vh;
-      display: flex;
-      justify-content: center;
-      align-items: center;
-      margin-top: -72px; /* Compensate for header height */
-    }
-    
-    h1 {
-      font-size: 3rem;
-      color: #1e88e5;
-      text-align: center;
-    }
-  `]
+  imports: [CommonModule],
+  templateUrl: './our-story.component.html',
+  styleUrls: ['./our-story.component.css']
 })
-export class OurStoryComponent {}
+export class OurStoryComponent {
+}
